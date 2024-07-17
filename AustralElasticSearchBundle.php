85ae3de1e82e14fbf6ffb8cba9ea13fa240f21bf
Matthieu Beurel <matthieu@mbeurel.com>
2024-07-17 09:23:57 +0200

    feat : Add compatibility Symfony 6.4

--- AustralElasticSearchBundle.php
+++ AustralElasticSearchBundle.php
@@ -23,7 +23,7 @@ class AustralElasticSearchBundle extends Bundle
   /**
    * @param ContainerBuilder $container
    */
-  public function build(ContainerBuilder $container)
+  public function build(ContainerBuilder $container): void
   {
     parent::build($container);
   }
